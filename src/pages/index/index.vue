d42d132817c384ddcfd35ef71108e05448da5959
jiannibang <271381693@qq.com>
2018-07-10 17:16:42 +0800

    init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,143 @@
+<template>
+  <div class="container" @click="clickHandle('test click', $event)">
+    <button v-if="!hasWxAuth" open-type="getUserInfo" bindgetuserinfo="handleUserInfo">登录</button> 
+    <div class="playground"></div>
+    <div class="buttons">
+      <div></div>
+      <button class="buttons-button" v-on:click="move('top')">上</button>
+      <div></div>
+      <button class="buttons-button" v-on:click="move('left')">左</button>
+      <button class="buttons-button" v-on:click="move('down')">下</button>
+      <button class="buttons-button" v-on:click="move('right')">右</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import card from "@/components/card";
+
+export default {
+  data() {
+    return {
+      motto: "Hello World",
+      userInfo: {},
+      hasWxAuth: false
+    };
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    move(direction) {},
+    async handleUserInfo({ detail: { encryptedData, iv, userInfo } }) {
+      if (userInfo) {
+        await this.openSocket();
+        await this.login(userInfo);
+      }
+    },
+    openSocket() {
+      return new Promise((resolve, reject) => {
+        wx.connectSocket({
+          url: "wss://archeryscorecalculator.com/wss",
+          data: {
+            x: "",
+            y: ""
+          },
+          header: {
+            "content-type": "application/json"
+          },
+          protocols: ["protocol1"],
+          method: "GET"
+        });
+        wx.onSocketOpen(function(res) {
+          resolve();
+          console.log("WebSocket连接已打开！");
+        });
+        wx.onSocketError(function(res) {
+          reject();
+          console.log("WebSocket连接打开失败，请检查！");
+        });
+      });
+    },
+    async init() {
+      this.hasWxAuth = await this.checkWxAuth();
+      if (this.hasWxAuth) {
+        let code = await this.getWxcode();
+        let { encryptedData, iv, userInfo } = await this.getUserInfo();
+        console.log(userInfo);
+        await this.openSocket();
+        let { user } = await this.login(userInfo);
+        this.currentUser = user;
+      }
+    },
+    login(userInfo) {
+      return new Promise(async resolve => {
+        this.hasWxAuth = true;
+        let code = await this.getWxcode();
+        this.currentUser = user;
+        resolve(this);
+      });
+    },
+    getWxcode() {
+      return new Promise(resolve => {
+        wx.login({
+          success: res => {
+            if (res.code) {
+              resolve(res.code);
+            } else {
+              console.log("登录失败！" + res.errMsg);
+            }
+          }
+        });
+      });
+    },
+    getUserInfo() {
+      return new Promise(resolve => {
+        wx.getUserInfo({
+          lang: "zh_CN",
+          success: res => {
+            resolve(res);
+          }
+        });
+      });
+    },
+    checkWxAuth() {
+      return new Promise(resolve => {
+        wx.getSetting({
+          success: res => {
+            resolve(res.authSetting["scope.userInfo"]);
+          }
+        });
+      });
+    }
+  },
+
+  created() {
+    // 调用应用实例的方法获取全局数据
+    this.init();
+  }
+};
+</script>
+
+<style scoped>
+.playground {
+  height: 80vh;
+  width: 100vw;
+  background: #f5f7f8;
+}
+.buttons {
+  height: 20vh;
+  width: 100vw;
+  display: grid;
+  grid-template-columns: 33% 33% 33%;
+}
+.buttons-button {
+  line-height: 18px;
+  padding-top: 28rpx;
+  padding-bottom: 28rpx;
+  border-radius: 50%;
+  margin: auto;
+}
+</style>
